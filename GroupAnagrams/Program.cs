1ff8950304c1f88de7e7d654f3300479b4148e6c
jose-engineer <jose-engineer@outlook.com>
2022-08-20 11:23:02 -0500

    Description

--- GroupAnagrams/Program.cs
+++ GroupAnagrams/Program.cs
@@ -3,6 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace GroupAnagrams {
+//    Given an array of strings strs, group the anagrams together.You can return the answer in any order.
+//An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+
+//Example 1:
+
+//Input: strs = ["eat","tea","tan","ate","nat","bat"]
+//    Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+//Example 2:
+
+//Input: strs = [""]
+//    Output: [[""]]
+//Example 3:
+
+//Input: strs = ["a"]
+//    Output: [["a"]]
+
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
@@ -18,9 +36,9 @@ namespace GroupAnagrams {
 
             foreach (var item in strs) {
 
-                char[] arr = item.ToCharArray(); //var ordered = item.OrderBy(x => x).ToArray();
+                char[] arr = item.ToCharArray(); 
                 Array.Sort(arr);    
-                string current = new string(arr);//string current = new string(ordered);
+                string current = new string(arr);
 
 
                 if (!hMap.ContainsKey(current)) {
