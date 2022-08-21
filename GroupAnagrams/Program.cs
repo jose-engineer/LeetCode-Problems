b35d10634b8536a2cad7609137e3c02cb351cc2b
jose-engineer <jose-engineer@outlook.com>
2022-08-21 14:48:05 -0500

    Added another approach

--- GroupAnagrams/Program.cs
+++ GroupAnagrams/Program.cs
@@ -4,29 +4,59 @@ using System.Linq;
 
 namespace GroupAnagrams {
 //    Given an array of strings strs, group the anagrams together.You can return the answer in any order.
-//An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
-
-
+//An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters
+//exactly once.
 
 //Example 1:
-
 //Input: strs = ["eat","tea","tan","ate","nat","bat"]
 //    Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
-//Example 2:
 
+//Example 2:
 //Input: strs = [""]
 //    Output: [[""]]
-//Example 3:
 
+//Example 3:
 //Input: strs = ["a"]
 //    Output: [["a"]]
 
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            List<IList<string>> lst1 = (List<IList<string>>)GroupAnagrams(new string[]{"eat","tea","tan","ate","nat","bat"});
+            List<IList<string>> lst2 = (List<IList<string>>)GroupAnagrams(new string[] { "" });
+            List<IList<string>> lst3 = (List<IList<string>>)GroupAnagrams(new string[] { "a" });
+
+            lst1.ForEach(x => x.ToList().ForEach(y => Console.WriteLine(y))); //Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+            lst2.ForEach(x => x.ToList().ForEach(y => Console.WriteLine(y))); //Output: [[""]]
+            lst3.ForEach(x => x.ToList().ForEach(y => Console.WriteLine(y))); //Output: [["a"]]
+
         }
 
-        public IList<IList<string>> GroupAnagrams(string[] strs) {
+        public static IList<IList<string>> GroupAnagrams(string[] strs) {
+
+            List<IList<string>> result = new List<IList<string>>();
+            Dictionary<string, List<string>> hMap = new Dictionary<string, List<string>>();
+
+            foreach (var item in strs) {
+                var ordered = item.OrderBy(x => x).ToArray();
+                string key = new string(ordered);
+
+                if (hMap.ContainsKey(key)) {
+                    hMap[key].Add(item);
+                } else {
+                    hMap[key] = new List<string>() { item };
+                }
+                
+            }
+
+            foreach (var item in hMap.Values) {
+                result.Add(item);
+            }
+
+            return result;
+        }
+
+        public IList<IList<string>> GroupAnagrams2(string[] strs) {
             if (strs.Length == 0) {
                 return new List<IList<string>>();
             }
@@ -55,32 +85,6 @@ namespace GroupAnagrams {
 
             return result;
         }
-
-        public static IList<IList<string>> GroupAnagrams2(string[] strs) {
-
-            List<IList<string>> result = new List<IList<string>>();
-
-            for (int i = 0; i < strs.Length; i++) {
-
-                char[] str1 = strs[i].ToCharArray();
-                Array.Sort(str1);
-
-                for (int j = i; j <= strs.Length - 1; j++) {
-
-                    char[] str2 = strs[j].ToCharArray();
-                    Array.Sort(str2);
-
-                    if (new string(str1) == new string(str2)) {
-                        Console.WriteLine("IN");
-                        //result[i][j] = strs[j];                    
-                    } else {
-                        continue;
-                    }
-
-                }
-            }
-
-            return result;
-        }
+        
     }
 }
