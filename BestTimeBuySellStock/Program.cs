621eb54c54fd174b6bc574dda3f0bd1aad400b59
jose-engineer <jose-engineer@outlook.com>
2022-11-10 20:49:24 -0600

    Buy Sell stock

---
+++ BestTimeBuySellStock/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Transactions;
+
+namespace BestTimeBuySellStock {
+
+//You are given an array prices where prices[i] is the price of a given stock on the ith day.
+//You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+//Return the maximum profit you can achieve from this transaction.If you cannot achieve any profit, return 0.
+
+//Example 1:
+//Input: prices = [7, 1, 5, 3, 6, 4]
+//Output: 5
+//Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+//Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+//Example 2:
+//Input: prices = [7,6,4,3,1]
+//    Output: 0
+//Explanation: In this case, no transactions are done and the max profit = 0.
+
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(MaxProfit(new int[] { 7, 1, 5, 3, 6, 4 })); //Expected: 5
+            Console.WriteLine(MaxProfit(new int[] { 7, 6, 4, 3, 1 })); //Expected: 0
+        }
+
+        public static int MaxProfit(int[] prices) {
+            int maxProfit = 0;
+            int min = prices.Max();
+
+            for (int i = 0; i < prices.Length; i++) {
+                if (prices[i] < min) {
+                    min = prices[i];
+                }
+
+                int profit = prices[i] - min; //calculating profit if sold today by, Buy - sell
+
+                if (profit > maxProfit) { //maxProfit = Math.Max(maxProfit, profit);
+                    maxProfit = profit;
+                }
+            }
+
+            return maxProfit;
+        }        
+
+        public static int MaxProfit2(int[] prices) {
+            int maxProfit = 0;
+
+            for (int i = 0; i < prices.Length - 1; i++) {
+                for (int j = i + 1; j < prices.Length; j++) {
+                    
+                    int profit = prices[j] - prices[i];
+
+                    if (profit > maxProfit) {
+                        maxProfit = profit;
+                    }                        
+                }
+            }
+
+            return maxProfit;
+        }
+
+        public int MaxProfit4(int[] prices) {
+            int start = 0, end = 1;
+            int maxProfit = 0;
+
+            while (end < prices.Length) {
+                if (prices[start] < prices[end]) {
+                    int profit = prices[end] - prices[start];
+                    maxProfit = Math.Max(maxProfit, profit);
+                } else {
+                    start = end;
+                }
+
+                end++;
+            }
+
+            return maxProfit;
+        }
+
+    }
+}
---
+++ FibonacciNumber/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FibonacciNumber {
+//    The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the
+//    two preceding ones, starting from 0 and 1. That is,
+
+//F(0) = 0, F(1) = 1
+//F(n) = F(n - 1) + F(n - 2), for n > 1.
+
+//Given n, calculate F(n). 
+
+//Example 1:
+//Input: n = 2
+//Output: 1
+//Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+//Example 2:
+//Input: n = 3
+//Output: 2
+//Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+
+//Example 3:
+//Input: n = 4
+//Output: 3
+//Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+//Constraints:
+//0 <= n <= 30
+
+    internal class Program {
+
+        public static Dictionary<int, int> hMap = new Dictionary<int, int>() { { 0, 0 }, { 1, 1 } }; //memory, cache
+
+        static void Main(string[] args) {
+            Console.WriteLine(Fib(2)); //Expected: 1
+            Console.WriteLine(Fib(3)); //Expected: 2
+        }
+
+        public static int Fib(int n) {
+            if (hMap.ContainsKey(n))
+                return hMap[n];
+
+            int result = Fib(n - 1) + Fib(n - 2);
+            hMap[n] = result;
+
+            return result;
+        }
+
+        public static int Fib2(int n) {
+            if (n <= 1)
+                return n;
+
+            return Fib2(n - 1) + Fib2(n - 2);
+        }
+
+        public static int Fib3(int n) {
+            List<int> arr = new List<int>(new int[n]); //int[] arr = new int[n];
+
+            if (n == 0)
+                return 0;
+
+            if (n == 1 || n == 2)
+                return 1;
+
+            arr[0] = 1;
+            arr[1] = 1;
+
+            for (int i = 2; i < n; i++) {
+                arr[i] = arr[i - 2] + arr[i - 1];
+            }
+
+            return arr[n - 1];
+        }        
+
+    }
+}
